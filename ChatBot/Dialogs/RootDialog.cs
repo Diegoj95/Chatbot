3db85a62315f600eb83b5a434661d3b679d56463
Diegoj95 <diegojimenez377@gmail.com>
2021-10-17 17:12:56 -0300

    Primer commit

---
+++ ChatBot/Dialogs/RootDialog.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Bot.Builder;
+using Microsoft.Bot.Builder.Dialogs;
+
+namespace ChatBot.Dialogs
+{
+    public class RootDialog : ComponentDialog
+    {
+        public RootDialog()
+        {
+            var waterfallStep = new WaterfallStep[]
+            {
+                SetName,
+                SetAge,           
+                ShowData
+            };
+
+            AddDialog(new WaterfallDialog(nameof(WaterfallDialog), waterfallStep));
+            AddDialog(new TextPrompt(nameof(TextPrompt)));
+            AddDialog(new NumberPrompt<int>(nameof(NumberPrompt<int>), ValidateAge));
+        }
+
+        private async Task<bool> ValidateAge(PromptValidatorContext<int> promptContext, CancellationToken cancellationToken)
+        {
+            return await Task.FromResult(
+                promptContext.Recognized.Succeeded &&
+                promptContext.Recognized.Value > 0 &&
+                promptContext.Recognized.Value < 150
+            );
+        }
+
+        private async Task<DialogTurnResult> SetName(WaterfallStepContext stepContext, CancellationToken cancellationToken)
+        {
+            await stepContext.Context.SendActivityAsync("Necesito algunos datos", cancellationToken: cancellationToken);
+            await Task.Delay(1000);
+            return await stepContext.PromptAsync(
+                nameof(TextPrompt),
+                new PromptOptions { Prompt = MessageFactory.Text("Por favor ingresa tu nombre")},
+                cancellationToken
+            );
+        }
+
+        private async Task<DialogTurnResult> SetAge(WaterfallStepContext stepContext, CancellationToken cancellationToken)
+        {
+            var name = stepContext.Context.Activity.Text;
+            return await stepContext.PromptAsync(
+                nameof(NumberPrompt<int>), 
+                new PromptOptions
+                {
+                    Prompt = MessageFactory.Text($"Bien {name}, ahora necesito tu edad"),
+                    RetryPrompt = MessageFactory.Text($"{name}, por favor ingresa una edad valida")
+                },
+                cancellationToken
+                );
+        }
+
+        private async Task<DialogTurnResult> ShowData(WaterfallStepContext stepContext, CancellationToken cancellationToken)
+        {
+            await stepContext.Context.SendActivityAsync("Genial, gracias por registrar tus datos.", cancellationToken: cancellationToken);
+            return await stepContext.EndDialogAsync(cancellationToken: cancellationToken);
+        }
+    }
+}
